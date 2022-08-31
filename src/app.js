b8b4aa2784677ce758c55e52d99cc2209c6a378d
Michaell Alavedra Munayco <michael.munayco@eficientis.com>
2022-08-30 22:03:53 -0400

    feat: generate pdf by volume

--- src/app.js
+++ src/app.js
@@ -6,32 +6,27 @@ const fs = require('fs');
 
 const MANGA_NAME = 'dragon-ball-super';
 const ASSETS_DIR = `./assets/${MANGA_NAME}/volumes`;
-const GENERATED_DIR = `./generated/${MANGA_NAME}`;
-const GENERATED_NAME = `${GENERATED_DIR}/${new Date().getTime()}.manga.pdf`;
+const GENERATED_DIR = `./generated/${MANGA_NAME}/volumes`;
 
 const loadingLogger = new cliProgress.MultiBar({
-  format: 'Volume {volumeName} [{bar}] {percentage}% | ETA: {eta}s | {value}/{total}',
+  format: 'Volume {volumeName}.pdf [{bar}] {percentage}% | ETA: {eta}s | {value}/{total}',
   clearOnComplete: false,
   hideCursor: true
 }, cliProgress.Presets.legacy);
 
 try {
 
-  if (!fs.existsSync(ASSETS_DIR)) {
-    throw new Error('No hay tomos agregados al folder assets');
-  }
-
   const volumeNames = fs.readdirSync(ASSETS_DIR);
 
   if (!volumeNames.length) {
-    throw new Error(`No se encontraron tomos. Proceso terminado!`);
+    throw new Error(`Volumes not found. Process finished!`);
   }
 
-  const pdf = new PDFDocument({ autoFirstPage: false, layout: 'landscape', size: 'A4', margin: 0 });
-
-  pdf.pipe(fs.createWriteStream(GENERATED_NAME));
-
   for (const volumeName of volumeNames) {
+
+    const pdf = new PDFDocument({ autoFirstPage: false, layout: 'landscape', size: 'A4', margin: 0 });
+
+    pdf.pipe(fs.createWriteStream(`${GENERATED_DIR}/${volumeName}.pdf`));
 
     const pageNames = fs.readdirSync(`${ASSETS_DIR}/${volumeName}`);
 
@@ -41,24 +36,23 @@ try {
 
     let i = 0;
     let j = pageNames.length - 1;
-    let loadValue = 0;
     let even = true;
 
     do {
 
-      const firstPageName = pageNames.at(i);
-      const lastPageName = pageNames.at(j);
+      const firstImage = pageNames.at(i);
+      const lastImage = pageNames.at(j);
 
       pdf.addPage();
       const imageWidth = pdf.page.width / 2;
       const fit = [imageWidth, pdf.page.height];
 
       if (even) {
-        pdf.image(`${ASSETS_DIR}/${volumeName}/${lastPageName}`, 0, 0, { fit });
-        pdf.image(`${ASSETS_DIR}/${volumeName}/${firstPageName}`, imageWidth, 0, { fit });
+        pdf.image(`${ASSETS_DIR}/${volumeName}/${lastImage}`, 0, 0, { fit });
+        pdf.image(`${ASSETS_DIR}/${volumeName}/${firstImage}`, imageWidth, 0, { fit });
       } else {
-        pdf.image(`${ASSETS_DIR}/${volumeName}/${firstPageName}`, 0, 0, { fit });
-        pdf.image(`${ASSETS_DIR}/${volumeName}/${lastPageName}`, imageWidth, 0, { fit });
+        pdf.image(`${ASSETS_DIR}/${volumeName}/${firstImage}`, 0, 0, { fit });
+        pdf.image(`${ASSETS_DIR}/${volumeName}/${lastImage}`, imageWidth, 0, { fit });
       }
 
       i++;
@@ -66,15 +60,13 @@ try {
 
       even = !even;
 
-      loadValue += 2;
-
-      loader.update(loadValue);
+      loader.increment(2);
 
     } while (i < j);
 
-  }
+    pdf.end();
 
-  pdf.end();
+  }
 
   loadingLogger.stop();
 
