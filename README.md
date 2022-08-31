a21dbd483821d08478d1bba55fbadd884e4e522b
Michaell Alavedra Munayco <michael.munayco@eficientis.com>
2022-08-30 21:14:56 -0400

    init

---
+++ README.md
@@ -0,0 +1,19 @@
+# Manga PDF Maker
+
+Generate a ordered-printable manga pdf from multiple images.
+
+## Steps
+
+1. En la carpeta `assets/` debes tener todas las paginas como imagen de los tomos del manga que deseas imprimir.
+1. Asegurate de que todas las imagenes tenga la rotacion correcta, para que leer sea mas facil.
+1. Asegurate de que todas las imagenes tenga como nombre el formato `000` segun la secuencia de lectura.
+1. Ejecutar el comando `npm start` y espera el archivo PDF generado.
+
+## Ejemplos Generados
+
+1. [./generated/dragon-ball-super/manga.pdf](./generated/dragon-ball-super/manga.pdf)
+
+## Recursos
+
+1. [Dragon Ball Super - All Volumes](https://tomosmanga.com/tomos-dragon-ball-super/)
+1. [Dragon Ball Super - Volume 1](https://www.terabox.com/sharing/link?surl=CQoHM9Wm-vSeFTQhWQ35Gw)
---
+++ src/app.js
@@ -0,0 +1,79 @@
+const cliProgress = require('cli-progress');
+const logger = require('pino')(require('pino-pretty')());
+const PDFDocument = require('pdfkit');
+const fs = require('fs');
+
+
+const MANGA_NAME = 'dragon-ball-super';
+const ASSETS_DIR = `./assets/${MANGA_NAME}/volumes`;
+const GENERATED_DIR = `./generated/${MANGA_NAME}`;
+const GENERATED_NAME = `${GENERATED_DIR}/${new Date().getTime()}.manga.pdf`;
+
+const loadingLogger = new cliProgress.MultiBar({
+  format: 'Volume {volumeName} [{bar}] {percentage}% | ETA: {eta}s | {value}/{total}',
+  clearOnComplete: false,
+  hideCursor: true
+}, cliProgress.Presets.legacy);
+
+try {
+
+  if (!fs.existsSync(ASSETS_DIR)) {
+    throw new Error('No hay tomos agregados al folder assets');
+  }
+
+  const volumeNames = fs.readdirSync(ASSETS_DIR);
+
+  if (!volumeNames.length) {
+    throw new Error(`No se encontraron tomos. Proceso terminado!`);
+  }
+
+  const pdf = new PDFDocument({ autoFirstPage: false, layout: 'landscape', size: 'A4', margin: 0 });
+
+  pdf.pipe(fs.createWriteStream(GENERATED_NAME));
+
+  for (const volumeName of volumeNames) {
+
+    const pageNames = fs.readdirSync(`${ASSETS_DIR}/${volumeName}`);
+
+    if (!pageNames.length) continue;
+
+    const loader = loadingLogger.create(pageNames.length, 0, { volumeName });
+
+    let i = 0;
+    let j = pageNames.length - 1;
+    let loadValue = 0;
+
+    do {
+
+      const firstPageName = pageNames.at(i);
+      const lastPageName = pageNames.at(j);
+
+      pdf
+        .addPage()
+        .image(`${ASSETS_DIR}/${volumeName}/${lastPageName}`, 0, 0,
+          {
+            fit: [pdf.page.width / 2, pdf.page.height]
+          })
+        .image(`${ASSETS_DIR}/${volumeName}/${firstPageName}`, pdf.page.width / 2, 0,
+          {
+            fit: [pdf.page.width / 2, pdf.page.height]
+          });
+
+      i++;
+      j--;
+
+      loadValue += 2;
+
+      loader.update(loadValue);
+
+    } while (i < j);
+
+  }
+
+  pdf.end();
+
+  loadingLogger.stop();
+
+} catch (err) {
+  logger.error(err.message);
+}
