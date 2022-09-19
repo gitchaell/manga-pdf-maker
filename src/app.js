53cd674f9744184f7c6233111bce3a002557992c
Michaell Alavedra Munayco <michael.munayco@eficientis.com>
2022-09-19 13:05:39 -0400

    feat: print mode adapter

--- src/app.js
+++ src/app.js
@@ -3,7 +3,8 @@ const fs = require('fs');
 const ProgressBar = require('progress');
 
 
-const MANGAS = ['dragon-ball-artbook', 'dragon-ball-super', 'jaco-patrullero-galactico'];
+// const MANGAS = ['dragon-ball-artbook', 'dragon-ball-super', 'jaco-patrullero-galactico'];
+const MANGAS = ['dragon-ball-super'];
 
 try {
 
@@ -14,43 +15,51 @@ try {
     const ASSETS_DIR = `./assets/${MANGA}/volumes`;
     const GENERATED_DIR = `./generated/${MANGA}/volumes`;
 
-    const volumeNames = fs.readdirSync(ASSETS_DIR);
+    const volumes = fs.readdirSync(ASSETS_DIR);
 
-    if (!volumeNames.length) {
+    if (!volumes.length) {
       throw new Error(`Volumes not found. Process finished!`);
     }
 
-    for (const volumeName of volumeNames) {
+    for (const volume of volumes) {
 
       const pdf = new PDFDocument({ autoFirstPage: false, layout: 'landscape', size: 'A4', margin: 0 });
 
-      pdf.pipe(fs.createWriteStream(`${GENERATED_DIR}/${volumeName}.pdf`));
+      pdf.pipe(fs.createWriteStream(`${GENERATED_DIR}/${volume}.pdf`));
 
-      const pageNames = fs.readdirSync(`${ASSETS_DIR}/${volumeName}`);
+      const pages = fs.readdirSync(`${ASSETS_DIR}/${volume}`);
 
-      if (!pageNames.length) continue;
+      if (!pages.length) {
+        throw new Error(`Pages not found in Volume: ${volume}`);
+      }
 
-      const loader = new ProgressBar('  Volume :volumeName [:bar] :rate/bps :percent', { total: pageNames.length });
+      if (pages.length % 4) {
+        throw new Error(`Total number of pages in Volume ${volume} (${pages.length}) must be multiples of 4`);
+      }
 
-      let i = 0;
-      let j = pageNames.length - 1;
+      const loader = new ProgressBar('  Volume :volume [:bar] :rate/bps :percent', { total: pages.length });
+
+      let i = 1;
+      let j = 16;
+      let iBase = i;
+      let jBase = j;
       let even = true;
 
       do {
 
-        const firstImage = pageNames.at(i);
-        const lastImage = pageNames.at(j);
+        const firstImage = pages.at(i - 1);
+        const lastImage = pages.at(j - 1);
 
         pdf.addPage();
         const imageWidth = pdf.page.width / 2;
         const fit = [imageWidth, pdf.page.height];
 
         if (even) {
-          pdf.image(`${ASSETS_DIR}/${volumeName}/${lastImage}`, 0, 0, { fit });
-          pdf.image(`${ASSETS_DIR}/${volumeName}/${firstImage}`, imageWidth, 0, { fit });
+          pdf.image(`${ASSETS_DIR}/${volume}/${lastImage}`, 0, 0, { fit });
+          pdf.image(`${ASSETS_DIR}/${volume}/${firstImage}`, imageWidth, 0, { fit });
         } else {
-          pdf.image(`${ASSETS_DIR}/${volumeName}/${firstImage}`, 0, 0, { fit });
-          pdf.image(`${ASSETS_DIR}/${volumeName}/${lastImage}`, imageWidth, 0, { fit });
+          pdf.image(`${ASSETS_DIR}/${volume}/${firstImage}`, 0, 0, { fit });
+          pdf.image(`${ASSETS_DIR}/${volume}/${lastImage}`, imageWidth, 0, { fit });
         }
 
         i++;
@@ -58,9 +67,19 @@ try {
 
         even = !even;
 
-        loader.tick(2, { volumeName });
+        loader.tick(2, { volume });
 
-      } while (i < j);
+        if (i - j === 1) {
+          iBase += 16;
+          jBase += 16;
+
+          if (iBase <= pages.length && jBase <= pages.length) {
+            i = iBase;
+            j = jBase;
+          } else break;
+        }
+
+      } while (true);
 
       pdf.end();
 
